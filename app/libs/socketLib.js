4daa301bb379d5ac00351a6f0bcc9f06b3c8fcad
pankajsaini123 <pankajsaini982134@gmail.com>
2019-01-02 13:13:11 +0530

    adding docs folder

--- app/libs/socketLib.js
+++ app/libs/socketLib.js
@@ -18,16 +18,35 @@ let setServer = (server) => {
         console.log("on connection--emitting verify user");
 
         socket.emit("verifyUser", "");
+           /**
+         * @api {emit} verifyUser verify user
+         * @apiVersion 1.0.0
+         * @apiGroup Emit 
+         *@apiDescription This event is emmited to verify asking user to verify itself.
+        */
         console.log('verify-user emitted from backend')
 
         // code to verify the user and make him online
 
         socket.on('set-user', (authToken) => {
+    /**
+     * @api {listen} set-user Setting user online
+     * @apiVersion 1.0.0
+     * @apiGroup Listen 
+     *@apiDescription This eventListner listens to user for authToken verification
+    */
+
 
             console.log("set-user called")
             tokenLib.verifyClaimWithoutSecret(authToken, (err, user) => {
                 if (err) {
                     socket.emit('auth-error', { status: 500, error: 'Please provide correct auth token' })
+            /**
+             * @api {emit} auth-error emit authentication error
+             * @apiVersion 1.0.0
+             * @apiGroup Emit 
+             *@apiDescription This event is emmited when the auth token provided by user cannot be verified
+            */
                 }
                 else {
 
@@ -52,14 +71,16 @@ let setServer = (server) => {
                                 } else {
 
                                     console.log(`${fullName} is online`);
-                                    //socket.room = 'LetsMeetRoom'
-                                    // joining global room.
-                                    //socket.join(socket.room) 
-
-                                    //socket.to(socket.room).broadcast.emit('online-user-list', result);
+                                    
                                     
  
                                     socket.broadcast.emit('online-user-list', result);
+                             /**
+                             * @api {emit} auth-error broadcast users online
+                             * @apiVersion 1.0.0
+                             * @apiGroup Emit 
+                             *@apiDescription This broadcast event is emited to indicate the online users
+                            */
                                 }
                             })
                         }
@@ -72,10 +93,13 @@ let setServer = (server) => {
 
 
         socket.on('disconnect', () => {
-            // disconnect the user from socket
-            // remove the user from online list
-            // unsubscribe the user from his own channel
-
+            
+             /**
+             * @api {listen} disconnect to disconnect from socket
+             * @apiVersion 1.0.0
+             * @apiGroup Listen 
+             *@apiDescription This eventListner is listens to disconnect event to disconnect from socket
+            */
             console.log("user is disconnected");
 
             if (socket.userId) {
@@ -94,6 +118,12 @@ let setServer = (server) => {
 
 
         socket.on('notify-updates', (data) => {
+               /**
+             * @api {listen} notify-updates notification
+             * @apiVersion 1.0.0
+             * @apiGroup Listen 
+             *@apiDescription This eventListner listens to updates from client
+            */
             console.log("socket notify-updates called")
             console.log(data);
             socket.broadcast.emit(data.userId, data);
--- eventDoc/api_project.js
+++ eventDoc/api_project.js
@@ -1,16 +1,15 @@
-{
+define({
   "name": "Todo Real Time Application",
   "version": "1.0.0",
   "description": "Documentation for Todo Real Time app",
   "title": "Todo API",
-  "url": "http://api.coolcoder.xyz",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-01-02T06:11:32.367Z",
+    "time": "2019-01-02T06:13:41.446Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
-}
+});
